01e545f25bdf03fc3504749d39fd8b48fa45d2a4
Joshua Shor <jpshor@gmail.com>
2015-06-20 16:44:41 -0400

    adds ics.js dependency and wrapper function, sets URLs to open in new tabs

--- addtocalendar.js
+++ addtocalendar.js
@@ -1,8 +1,11 @@
 angular.module('jshor.angular-addtocalendar', [])
 	.controller('AddtocalendarCtrl', function($scope) {
-		var getIcs = function() {
+		var cal = ics();
 
-		};var obj = { first: "John", last: "Doe" };
+		$scope.getIcsCalendarUrl = function() {
+			cal.addEvent($scope.title, $scope.description, $scope.location, $scope.startDate, $scope.endDate);
+			return cal.download();
+		};
 
 		$scope.getYahooCalendarUrl = function() {
 			var yahooCalendarUrl = 'http://calendar.yahoo.com/?v=60&view=d&type=20';
@@ -10,8 +13,8 @@ angular.module('jshor.angular-addtocalendar', [])
 			yahooCalendarUrl += '&st=' + encodeURI($scope.startDate) + '&et=' + encodeURI($scope.endDate);
 			yahooCalendarUrl += '&desc=' + encodeURI($scope.description);
 			yahooCalendarUrl += '&in_loc=' + encodeURI($scope.location);
-			console.log('yahooCalendarUrl: ', yahooCalendarUrl);
-			return yahooCalendarUrl;
+
+			return window.open(yahooCalendarUrl);
 		};
 
 		$scope.getGoogleCalendarUrl = function() {
@@ -20,8 +23,8 @@ angular.module('jshor.angular-addtocalendar', [])
 			googleCalendarUrl += '&dates=' + encodeURI($scope.startDate) + '/' + encodeURI($scope.endDate);
 			googleCalendarUrl += '&details=' + encodeURI($scope.description);
 			googleCalendarUrl += '&location=' + encodeURI($scope.location);
-			console.log('googleCalendarUrl: ', googleCalendarUrl);
-			return googleCalendarUrl;
+
+			return window.open(googleCalendarUrl);
 		};
 
 		$scope.getMicrosoftCalendarUrl = function() {
@@ -30,12 +33,11 @@ angular.module('jshor.angular-addtocalendar', [])
 			microsoftCalendarUrl += '&dtstart=' + encodeURI($scope.startDate) + '&dtend=' + encodeURI($scope.endDate);
 			microsoftCalendarUrl += '&description=' + encodeURI($scope.description);
 			microsoftCalendarUrl += '&location=' + encodeURI($scope.location);
-			console.log('microsoftCalendarUrl: ', microsoftCalendarUrl);
-			return microsoftCalendarUrl;
+
+			return window.open(microsoftCalendarUrl);
 		};
 	})
 	.directive('addtocalendar', function() {
-
     return {
       restrict: 'E',
       scope: {
@@ -46,6 +48,6 @@ angular.module('jshor.angular-addtocalendar', [])
         location: '@' 
       },
     	controller: 'AddtocalendarCtrl',
-      template: '<div><a href="#" ng-click="getGoogleCalendarUrl()">google calendar</a><br /><a href="#" ng-click="getYahooCalendarUrl()">yahoo calendar</a><br /><a href="#" ng-click="getMicrosoftCalendarUrl()">microsoft calendar</a><br /></div>'
+      template: '<div><a href="#" ng-click="getIcsCalendarUrl()">ICS</a><br /><a href="#" ng-click="getGoogleCalendarUrl()">google calendar</a><br /><a href="#" ng-click="getYahooCalendarUrl()">yahoo calendar</a><br /><a href="#" ng-click="getMicrosoftCalendarUrl()">microsoft calendar</a><br /></div>'
     };
 	});
