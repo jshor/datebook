e78fc0dd0df59f75c6c4b368193dd9f170318040
Joshua Shor <jpshor@gmail.com>
2015-06-20 15:41:09 -0400

    adds directive controller, adds URL creator for Google, Yahoo! and Microsoft calendars

--- addtocalendar.js
+++ addtocalendar.js
@@ -1,10 +1,51 @@
-angular.module('jshor.angular-addtocalendar', []).
-	directive('ngAddtocalendar', function () {
+angular.module('jshor.angular-addtocalendar', [])
+	.controller('AddtocalendarCtrl', function($scope) {
+		var getIcs = function() {
+
+		};var obj = { first: "John", last: "Doe" };
+
+		$scope.getYahooCalendarUrl = function() {
+			var yahooCalendarUrl = 'http://calendar.yahoo.com/?v=60&view=d&type=20';
+			yahooCalendarUrl += '&title=' + encodeURI($scope.title);
+			yahooCalendarUrl += '&st=' + encodeURI($scope.startDate) + '&et=' + encodeURI($scope.endDate);
+			yahooCalendarUrl += '&desc=' + encodeURI($scope.description);
+			yahooCalendarUrl += '&in_loc=' + encodeURI($scope.location);
+			console.log('yahooCalendarUrl: ', yahooCalendarUrl);
+			return yahooCalendarUrl;
+		};
+
+		$scope.getGoogleCalendarUrl = function() {
+			var googleCalendarUrl = 'https://www.google.com/calendar/render?action=TEMPLATE';
+			googleCalendarUrl += '&text=' + encodeURI($scope.title);
+			googleCalendarUrl += '&dates=' + encodeURI($scope.startDate) + '/' + encodeURI($scope.endDate);
+			googleCalendarUrl += '&details=' + encodeURI($scope.description);
+			googleCalendarUrl += '&location=' + encodeURI($scope.location);
+			console.log('googleCalendarUrl: ', googleCalendarUrl);
+			return googleCalendarUrl;
+		};
+
+		$scope.getMicrosoftCalendarUrl = function() {
+			var microsoftCalendarUrl = 'http://calendar.live.com/calendar/calendar.aspx?rru=addevent';
+			microsoftCalendarUrl += '&summary=' + encodeURI($scope.title);
+			microsoftCalendarUrl += '&dtstart=' + encodeURI($scope.startDate) + '&dtend=' + encodeURI($scope.endDate);
+			microsoftCalendarUrl += '&description=' + encodeURI($scope.description);
+			microsoftCalendarUrl += '&location=' + encodeURI($scope.location);
+			console.log('microsoftCalendarUrl: ', microsoftCalendarUrl);
+			return microsoftCalendarUrl;
+		};
+	})
+	.directive('addtocalendar', function() {
+
     return {
       restrict: 'E',
       scope: {
-        eventDate: '@'
+        startDate: '@',
+        endDate: '@',
+        title: '@',
+        description: '@',
+        location: '@' 
       },
-      template: '<div>Date: {{eventDate}}</div>'
+    	controller: 'AddtocalendarCtrl',
+      template: '<div><a href="#" ng-click="getGoogleCalendarUrl()">google calendar</a><br /><a href="#" ng-click="getYahooCalendarUrl()">yahoo calendar</a><br /><a href="#" ng-click="getMicrosoftCalendarUrl()">microsoft calendar</a><br /></div>'
     };
-});
+	});
