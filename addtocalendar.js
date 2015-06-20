a8a31f55ee1e95e7e2d66c3c4595f6a62e0cf35d
Joshua Shor <jpshor@gmail.com>
2015-06-20 12:22:53 -0400

    adds node_modules to gitignore, creates demo and initial directive structure

--- addtocalendar.js
+++ addtocalendar.js
@@ -1,4 +1,10 @@
 angular.module('jshor.angular-addtocalendar', []).
 	directive('ngAddtocalendar', function () {
-		
+    return {
+      restrict: 'E',
+      scope: {
+        eventDate: '@'
+      },
+      template: '<div>Date: {{eventDate}}</div>'
+    };
 });
