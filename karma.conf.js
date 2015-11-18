de56b7a8da1aeedc00c2452ecbcfc902a9d80a6e
Josh Shor <jpshor@gmail.com>
2015-11-18 11:28:45 -0500

    adds firefox to testing bundle for travis

--- karma.conf.js
+++ karma.conf.js
@@ -73,7 +73,11 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
+    browsers: [
+      'Chrome',
+      'Firefox',
+      'PhantomJS'
+    ],
 
 
     // Continuous Integration mode
