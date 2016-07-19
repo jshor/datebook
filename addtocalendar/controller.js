acbf0bc7dce403f19dbd573af436d65ef2413593
Josh S <jpshor@gmail.com>
2016-07-19 11:18:24 -0400

    updates readme installation instructions, adds new start/end-time params; time-formatting functions

--- addtocalendar/controller.js
+++ addtocalendar/controller.js
@@ -7,8 +7,10 @@
 'use strict';
 
 addtocalendar
-  .controller('AddtocalendarCtrl', ['$scope', '$attrs', 'FileSaver', 'Blob',
-    function ($scope, $attrs, FileSaver, Blob) {
+  .controller('AddtocalendarCtrl', ['$scope', '$attrs',
+    function ($scope, $attrs) {
+
+      var dates = {};
 
       $scope.description = $scope.description || '';
 
@@ -23,8 +25,8 @@ addtocalendar
           'BEGIN:VEVENT',
           'CLASS:PUBLIC',
           'DESCRIPTION:' + formatIcsText($scope.description, 62),
-          'DTSTART:' + $scope.startDate,
-          'DTEND:' + $scope.endDate,
+          'DTSTART:' + dates.startDate,
+          'DTEND:' + dates.endDate,
           'LOCATION:' + formatIcsText($scope.location, 64),
           'SUMMARY:' + formatIcsText($scope.title, 66),
           'TRANSP:TRANSPARENT',
@@ -32,12 +34,24 @@ addtocalendar
           'END:VCALENDAR'
         ].join('\n');
       }
-      
+
+      function setTimesFromFormat() {
+        var format = $scope.format;
+        var timezone = $scope.timezone;
+
+        ['startDate', 'endDate']
+          .forEach(function(t) {
+            dates[t] = formatTime($scope[t], format, timezone);
+          });
+      }
+
       function getYahooCalendarUrl() {
         var yahooCalendarUrl = 'http://calendar.yahoo.com/?v=60&view=d&type=20';
-        yahooCalendarUrl += '&title=' + encodeURIComponent($scope.title);
-        yahooCalendarUrl += '&st=' + encodeURIComponent($scope.startDate) + '&et=' + encodeURIComponent($scope.endDate);
-        yahooCalendarUrl += '&desc=' + encodeURIComponent($scope.description);
+        var duration = getHoursDuration($scope.startDate, $scope.endDate);
+
+        yahooCalendarUrl += '&TITLE=' + encodeURIComponent($scope.title);
+        yahooCalendarUrl += '&ST=' + encodeURIComponent(dates.startDate) + '&DUR=' + encodeURIComponent(duration);
+        yahooCalendarUrl += '&DESC=' + encodeURIComponent($scope.description);
         yahooCalendarUrl += '&in_loc=' + encodeURIComponent($scope.location);
 
         return yahooCalendarUrl;
@@ -46,7 +60,7 @@ addtocalendar
       function getGoogleCalendarUrl() {
         var googleCalendarUrl = 'https://www.google.com/calendar/render?action=TEMPLATE';
         googleCalendarUrl += '&text=' + encodeURIComponent($scope.title);
-        googleCalendarUrl += '&dates=' + encodeURIComponent($scope.startDate) + '/' + encodeURIComponent($scope.endDate);
+        googleCalendarUrl += '&dates=' + encodeURIComponent(dates.startDate) + '/' + encodeURIComponent(dates.endDate);
         googleCalendarUrl += '&details=' + encodeURIComponent($scope.description);
         googleCalendarUrl += '&location=' + encodeURIComponent($scope.location);
 
@@ -56,27 +70,22 @@ addtocalendar
       function getMicrosoftCalendarUrl() {
         var microsoftCalendarUrl = 'http://calendar.live.com/calendar/calendar.aspx?rru=addevent';
         microsoftCalendarUrl += '&summary=' + encodeURIComponent($scope.title);
-        microsoftCalendarUrl += '&dtstart=' + encodeURIComponent($scope.startDate) + '&dtend=' + encodeURIComponent($scope.endDate);
+        microsoftCalendarUrl += '&dtstart=' + encodeURIComponent(dates.startDate) + '&dtend=' + encodeURIComponent(dates.endDate);
         microsoftCalendarUrl += '&description=' + encodeURIComponent($scope.description);
         microsoftCalendarUrl += '&location=' + encodeURIComponent($scope.location);
 
         return microsoftCalendarUrl;
       }
 
-      function canDownloadIcal() {
-        return !!new Blob;
-      }
-
       function buildUrl() {
+        setTimesFromFormat();
         $scope.calendarUrl = {
           microsoft: getMicrosoftCalendarUrl(),
           google: getGoogleCalendarUrl(),
           yahoo: getYahooCalendarUrl(),
-          icalendar: getIcsCalendar()
+          icalendar: getIcsCalendar(),
+          dlIcal: dlIcal
         };
-        if(canDownloadIcal()) {
-          $scope.calendarUrl.dlIcal = dlIcal;
-        }
       }
 
       /**
@@ -85,11 +94,8 @@ addtocalendar
        * having the extension `.ics`.
        */
       function dlIcal() {
-        var fileName = $scope.title.replace(/[^\w ]+/g, '');
-        var blob = new Blob([getIcsCalendar()], {
-          type: 'application/octet-stream'
-        });
-        FileSaver.saveAs(blob, fileName + '.ics');
+        var fileName = $scope.title.replace(/[^\w ]+/g, '') + '.ics';
+        download(getIcsCalendar(), fileName, 'application/octet-stream');
       }
 
       // observe user-specified attributes
@@ -98,7 +104,6 @@ addtocalendar
           buildUrl();
         });
       });
-
       buildUrl();
     }
   ]);
