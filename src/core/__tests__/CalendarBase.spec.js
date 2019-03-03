9da885fcb2570bf79ea0b33a4af815fa60e20b9a
Chris Sakai <cs.chris.sakai@gmail.com>
2019-03-03 14:30:15 -0500

    Add tests for CalendarBase.setTimestamps()

--- src/core/__tests__/CalendarBase.spec.js
+++ src/core/__tests__/CalendarBase.spec.js
@@ -1,3 +1,5 @@
+import moment from 'moment'
+import { FORMAT } from '../../constants'
 import CalendarBase from '../CalendarBase'
 
 describe('Calendar Base', () => {
@@ -61,6 +63,94 @@ describe('Calendar Base', () => {
     })
   })
 
-  describe('setTimestamps', () => {
+  describe('setTimestamps()', () => {
+    const dateFormat = FORMAT.DATE
+    let calendarObj
+
+    beforeEach(() => {
+      calendarObj = new CalendarBase({})
+    })
+
+    it('should set the recurrence', () => {
+      const testOpts = {
+        recurrence: {}
+      }
+
+      calendarObj.setTimestamps(testOpts)
+
+      expect(calendarObj.recurrence).toBe(testOpts.recurrence)
+    })
+
+    describe('when options has no end', () => {
+
+      it('should set allday to true', () => {
+        calendarObj.setTimestamps({
+          start: '20150704T190000',
+          end: ''
+        });
+
+        expect(calendarObj.allday).toBe(true);
+      })
+
+      it('should set the end using the start + 1 day', () => {
+        const testOpts = {
+          start: '20150704T190000',
+          end: ''
+        }
+
+        const expectedEnd = moment(testOpts.start)
+          .add(1, 'days')
+          .format(dateFormat)
+
+        calendarObj.setTimestamps(testOpts)
+        expect(calendarObj.end).toBe(expectedEnd)
+      })
+
+      it('should set the start and end without the time of day', () => {
+        const dateTimeFormat = `${dateFormat}T${FORMAT.TIME}`
+        const testOpts = {
+          start: '20150704T190000',
+          end: ''
+        }
+
+        const expectedStart = moment(testOpts.start).format(dateFormat)
+        const expectedEnd = moment(testOpts.start)
+          .add(1, 'days')
+          .format(dateFormat)
+
+        calendarObj.setTimestamps(testOpts)
+
+        expect(calendarObj.start).toBe(expectedStart)
+        expect(calendarObj.end).toBe(expectedEnd)
+      })
+    })
+
+    describe('when options has an end', () => {
+
+      it('should set allday to false', () => {
+        calendarObj.setTimestamps({
+          start: '20150704T190000',
+          end: '20150704T200000'
+        });
+
+        expect(calendarObj.allday).toBe(false);
+      })
+
+      it('should set the start and end including the time of day', () => {
+        const dateTimeFormat = `${dateFormat}T${FORMAT.TIME}`
+        const testOpts = {
+          start: '20150704T190000',
+          end: '20150704T220000'
+        }
+
+        const expectedStart = moment(testOpts.start).format(dateTimeFormat)
+        const expectedEnd = moment(testOpts.end).format(dateTimeFormat)
+
+        calendarObj.setTimestamps(testOpts)
+
+        expect(calendarObj.start).toBe(expectedStart)
+        expect(calendarObj.end).toBe(expectedEnd)
+      })
+    })
   })
 })
