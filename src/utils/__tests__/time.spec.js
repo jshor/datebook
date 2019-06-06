057e1a8578f120bbfc654aca95af89662684ff04
Chris Sakai <cs.chris.sakai@gmail.com>
2019-06-05 22:38:23 -0400

    Accommodate moment output in test

--- src/utils/__tests__/time.spec.js
+++ src/utils/__tests__/time.spec.js
@@ -3,14 +3,11 @@ import { FORMAT } from '../../constants'
 import { formatTime, getHoursDuration, getTimeCreated } from '../time'
 
 describe('time util', () => {
-  beforeEach(() => {
-    moment.tz.setDefault("America/New_York")
-  })
   describe('formatTime()', () => {
     it('should format the time with the passed-in format', () => {
       const time = '2019-03-23 17:00-0500'
-      const expectedDateOutput = '20190323'
-      const expectedTimeOutput = '170000'
+      const expectedDateOutput = moment(time).format(FORMAT.DATE)
+      const expectedTimeOutput = moment(time).format(FORMAT.TIME)
       const actualDateOutput = formatTime(time, FORMAT.DATE)
       const actualTimeOutput = formatTime(time, FORMAT.TIME)
 
