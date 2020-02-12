f3e00899b6d4a89167c4eabd97e8c7812a88c752
David Eglin <djeglin@gmail.com>
2020-02-12 12:02:04 +0000

    Update description cutoff

--- src/ICalendar.js
+++ src/ICalendar.js
@@ -62,7 +62,7 @@ export default class ICalendar extends CalendarBase {
    * @returns {String}
    */
   render () {
-    const description = formatText(this.description, 62)
+    const description = formatText(this.description, 512)
     const location = formatText(this.location, 64)
     const summary = formatText(this.title, 66)
     const event = [
