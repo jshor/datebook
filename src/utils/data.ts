e84c73ebad41f42d67d3a8e45b2d46de37722e34
jshor <jpshor@gmail.com>
2022-11-25 09:13:11 -0500

    fix(recurrence): adds missing months param
    Adds months param to recurrence. Corrects monthdays param type from
    string to number[].

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,18 +1,18 @@
 import CalendarAttendee from '../types/CalendarAttendee'
 
-const identity = (str: string): string => str
-
-type Identity = (str: string) => string
-
 /**
  * Creates a param string from a flat key-value pair.
  *
  * @param {Record<string, string>} params
  * @param {string} [delimiter = ';']
- * @param {Function} [transformFn = identity] - transformation function to be applied to each param
+ * @param {Function} [transformFn] - transformation function to be applied to each param
  * @returns {string}
  */
-const toParamString = (params: Record<string, string>, delimiter = ';', transformFn: Identity = identity): string => {
+const toParamString = (
+  params: Record<string, string>,
+  delimiter = ';',
+  transformFn: (s: string) => string = s => s
+): string => {
   const paramString = []
 
   for (const key in params) {
