24d5a107322dbf766b67663aaca8b41de777e35b
jshor <jpshor@gmail.com>
2022-11-25 19:01:03 -0500

    feat(module): changes build from UMD to SystemJS #181, #203
    This change replaces Webpack/UMD build generation with SystemJS.

---
+++ src/modules.d.ts
@@ -0,0 +1,17 @@
+import {
+  GoogleCalendar,
+  YahooCalendar,
+  OutlookCalendar,
+  ICalendar
+} from '.'
+
+declare global {
+  interface Window {
+    datebook: {
+      GoogleCalendar
+      YahooCalendar
+      OutlookCalendar
+      ICalendar
+    }
+  }
+}
